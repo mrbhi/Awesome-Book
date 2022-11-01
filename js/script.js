6682dd57dd0f2bb2dea40f1c7b80f91aee477569
mrbhi <taofeeqworld@hotmail.com>
2022-11-01 08:04:01 -0700

    Added the delete from storage and UI functionality

--- js/script.js
+++ js/script.js
@@ -1,96 +1,65 @@
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
-
-class Storage {
-  static getBooks() {
-    let books;
-    if (localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
-
-    return books;
-  }
-
-  static addBook(book) {
-    const books = Storage.getBooks();
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-
-  static removeBook(title) {
-    const books = Storage.getBooks();
-
-    books.forEach((book, index) => {
-      if (book.title === title) {
-        books.splice(index, 1);
-      }
-    });
-
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-}
-const defaultBooks = Storage.getBooks();
-class Show {
-  static removeBook(target) {
-    if (target.classList.contains('delete')) {
-      target.parentElement.remove();
-    }
-  }
-
-  static clearFields() {
-    const title = document.querySelector('#title');
-    const author = document.querySelector('#author');
-    title.value = '';
-    author.value = '';
-  }
-
-  static showBooks() {
-    defaultBooks.forEach((data) => Show.addBookToList(data));
-  }
-
-  static addBookToList(data) {
-    const bookSection = document.querySelector('#data');
-    const render = document.createElement('div');
-    render.innerHTML = `
-    <span>${data.title}</span> <br>
-    <span>${data.author}</span> <br>
-    <button id="delete" class="delete">remove</button>
-    <hr color="black" size="1px" />`;
-    bookSection.appendChild(render);
-  }
-}
-
-Show.showBooks();
-
-function removeData(e) {
-  Show.removeBook(e.target);
-}
-
-function addABook(e) {
-  e.preventDefault();
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-
-  const book = new Book(title, author);
-
-  // Add book to Display
-  Show.addBookToList(book);
-
-  // Add book to list
-
-  Storage.addBook(book);
-
-  // Clear input box after entering new book
-  Show.clearFields();
-}
-
 const form = document.querySelector('#form');
-form.addEventListener('submit', addABook);
-const bookData = document.getElementById('data');
-bookData.addEventListener('click', removeData);
+const data = document.querySelector('#data');
+
+function addNewBookData() {
+  const bookTitle = form.title.value;
+  const bookAuthor = form.author.value;
+
+  const book = {
+    title: bookTitle,
+    author: bookAuthor,
+  };
+
+  return book;
+}
+
+function saveBooks(book) {
+  let books = [];
+
+  if (localStorage.getItem('Books')) {
+    books = JSON.parse(localStorage.getItem('Books'));
+  }
+
+  books.push(book);
+  localStorage.setItem('Books', JSON.stringify(books));
+}
+
+function addBook(element) {
+  element.preventDefault();
+  saveBooks(addNewBookData());
+  form.submit();
+}
+
+function showBooks() {
+  if (localStorage.getItem('Books')) {
+    const books = JSON.parse(localStorage.getItem('Books'));
+    books.forEach((book) => {
+      const booksHtml = `
+        <div id="info" class="info">
+          <span class="title">${book.title}</span> <br>
+          <span class="author">${book.author}</span> <br>
+          <span><button class="remove-btn">Remove</button></span> <br><br>
+        </div>
+      `;
+      data.innerHTML += booksHtml;
+    });
+  }
+}
+
+showBooks();
+form.addEventListener('submit', addBook);
+function removeBook(index) {
+  if (localStorage.getItem('Books')) {
+    const books = JSON.parse(localStorage.getItem('Books'));
+    books.splice(index, 1);
+    localStorage.clear();
+    localStorage.setItem('Books', JSON.stringify(books));
+  }
+}
+
+data.querySelectorAll('.remove-btn').forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    removeBook(index);
+    btn.parentElement.parentElement.remove();
+  });
+});
